383f80204fa2518d995cbad3e12d1eaeb96f725b
Tucker Hembree <bladeofxp@gmail.com>
2020-03-17 20:08:08 -0400

    added Bootstrap, JQuery, and linked style and script files; added basic header and container structures with HTML and Bootstrap; customized header to center text with CSS; used JQuery and DOM manipulation through a for loop to create an auto-populating list based upon input captured from a Bootstrap input form; currently working on accessing OpenWeather API data through ajax - information is coming through, but need to modify access method to account for input field vs data-name from captured buttons

---
+++ assets/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+  var searchedCities = [];
+
+  function displayCurrentWeather() {
+    var city = $(this).attr("data-name");
+    var queryURL =
+      "api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&apiid=e1eb6c55373004adbe9f3bca057c7ee4";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log(response); 
+    });
+    console.log(city); 
+  }
+
+  function displayFutureWeather() {
+    var city = $(this).attr("data-name");
+    var queryURL =
+      "api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&apiid=e1eb6c55373004adbe9f3bca057c7ee4";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log(response); 
+    });
+    console.log(city); 
+  }
+  function cityList() {
+    $("#city-list").empty();
+
+    for (i = 0; i < searchedCities.length; i++) {
+      var savedCities = $("<button>");
+      savedCities.addClass("city");
+      savedCities.addClass("list-group-item");
+      savedCities.attr("data-name", searchedCities[i]);
+      savedCities.text(searchedCities[i]);
+      $("#city-list").append(savedCities);
+    }
+  }
+
+  $("#search-button").on("click", function(event) {
+    event.preventDefault();
+    var city = $("#city-input")
+      .val()
+      .trim();
+
+    searchedCities.push(city);
+    console.log(searchedCities);
+
+    cityList();
+    displayCurrentWeather(); 
+    displayFutureWeather(); 
+  });
+
+  $(".city").on("click", function(event){ 
+      event.preventDefault(); 
+      displayCurrentWeather(); 
+      displayFutureWeather();   
+  })
+
+  cityList();
+});
